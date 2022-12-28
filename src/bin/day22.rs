f99bf629084ee75ba44ad92f60efc102ca6372ed
David Simon <david.simon@color.com>
2022-12-28 14:57:26 -0800

    wip

--- src/bin/day22.rs
+++ src/bin/day22.rs
@@ -10,11 +10,13 @@ use std::{
 
 use itertools::Itertools;
 
-#[derive(Clone, Copy, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
 enum Spot {
     Blank,
     Floor,
     Wall,
+    Portal(char),
+    PortalStart(char),
 }
 
 impl From<char> for Spot {
@@ -23,20 +25,30 @@ impl From<char> for Spot {
             ' ' => Spot::Blank,
             '.' => Spot::Floor,
             '#' => Spot::Wall,
-            _ => panic!("Unknown spot character"),
+            c => {
+                if c.is_ascii_uppercase() {
+                    Spot::Portal(c)
+                } else if c.is_ascii_lowercase() {
+                    Spot::PortalStart(c.to_ascii_uppercase())
+                } else {
+                    panic!("Unknown character {}", c)
+                }
+            }
         }
     }
 }
 
-impl Debug for Spot {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Blank => write!(f, " "),
-            Self::Floor => write!(f, "."),
-            Self::Wall => write!(f, "#"),
-        }
-    }
-}
+// impl Debug for Spot {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             Self::Blank => write!(f, " "),
+//             Self::Floor => write!(f, "."),
+//             Self::Wall => write!(f, "#"),
+//             Self::Portal(c) => write!(f, "{}", c),
+//             Self::PortalStart(c) => write!(f, "{}", c.to_ascii_lowercase()),
+//         }
+//     }
+// }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 enum Instruction {
@@ -75,6 +87,15 @@ impl Direction {
     fn turn_ccw(&self) -> Self {
         self.turn_cw().turn_cw().turn_cw()
     }
+
+    fn to_offset(&self) -> (isize, isize) {
+        match self {
+            Direction::Up => (0, -1),
+            Direction::Down => (0, 1),
+            Direction::Left => (-1, 0),
+            Direction::Right => (1, 0),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
@@ -87,26 +108,21 @@ struct State {
 impl State {
     fn start(map: &Vec<Vec<Spot>>) -> Self {
         let (x, _) = map
-            .get(0)
+            .get(1)
             .unwrap()
             .iter()
             .find_position(|&&spot| spot == Spot::Floor)
             .unwrap();
         State {
             x,
-            y: 0,
+            y: 1,
             facing: Direction::Right,
         }
     }
 
     fn apply_instruction(self, i: Instruction, map: &Vec<Vec<Spot>>) -> Self {
         match i {
-            Instruction::Forward(n) => match self.facing {
-                Direction::Up => self.move_steps(n, 0, -1, map),
-                Direction::Down => self.move_steps(n, 0, 1, map),
-                Direction::Left => self.move_steps(n, -1, 0, map),
-                Direction::Right => self.move_steps(n, 1, 0, map),
-            },
+            Instruction::Forward(n) => self.move_steps(n, self.facing, map),
             Instruction::Clockwise => Self {
                 facing: self.facing.turn_cw(),
                 ..self
@@ -118,58 +134,90 @@ impl State {
         }
     }
 
-    fn move_steps(
-        self,
-        steps: usize,
-        x_offset: isize,
-        y_offset: isize,
-        map: &Vec<Vec<Spot>>,
-    ) -> Self {
-        let width: isize = map.get(0).unwrap().len() as isize;
-        let height: isize = map.len() as isize;
-
+    fn move_steps(self, steps: usize, facing: Direction, map: &Vec<Vec<Spot>>) -> Self {
+        let offset = facing.to_offset();
         let mut result = self;
+
         for _ in 0..steps {
             let mut next_x = result.x as isize;
             let mut next_y = result.y as isize;
             loop {
-                next_x += x_offset;
-                if next_x < 0 {
-                    next_x = width - 1;
-                } else if next_x >= width {
-                    next_x = 0;
-                }
+                next_x += offset.0;
+                next_y += offset.1;
 
-                next_y += y_offset;
-                if next_y < 0 {
-                    next_y = height - 1;
-                } else if next_y >= height {
-                    next_y = 0;
-                }
-
-                let tgt_spot = *map
+                let tgt_spot = map
                     .get(next_y as usize)
-                    .unwrap()
-                    .get(next_x as usize)
-                    .unwrap();
+                    .map(|row| row.get(next_x as usize))
+                    .unwrap_or(None);
 
                 match tgt_spot {
-                    Spot::Blank => continue,
-                    Spot::Floor => {
+                    None | Some(Spot::Blank) => {
+                        panic!("Fell off the edge of the world")
+                    }
+                    Some(Spot::Floor) => {
                         result.x = next_x as usize;
                         result.y = next_y as usize;
                         break;
                     }
-                    Spot::Wall => return result,
+                    Some(Spot::Wall) => return result,
+                    Some(Spot::Portal(c) | Spot::PortalStart(c)) => {
+                        result = portal_teleport(
+                            result,
+                            (next_x, next_y),
+                            *tgt_spot.unwrap(),
+                            facing,
+                            map,
+                        );
+                    }
                 }
             }
         }
+
         result
     }
 
     fn to_score(&self) -> usize {
-        1000 * (self.y + 1) + 4 * (self.x + 1) + self.facing.to_score()
+        1000 * (self.y) + 4 * (self.x) + self.facing.to_score()
     }
+}
+
+fn portal_teleport(
+    state: State,
+    next_pos: (isize, isize),
+    tgt_spot: Spot,
+    facing: Direction,
+    map: &Vec<Vec<Spot>>,
+) -> State {
+    let (portal_start, portal_offset): ((isize, isize), usize) = match tgt_spot {
+        Spot::PortalStart(_) => (next_pos, 0),
+        Spot::Portal(c) => {
+            match facing {
+                Direction::Left | Direction::Right => {
+                    let r = map
+                        .iter()
+                        .map(|row| row.get(next_pos.0 as usize))
+                        .enumerate()
+                        .find(|(_, s)| s == &Some(&Spot::PortalStart(c)) )
+                        .unwrap();
+                    ((next_pos.0, r.0 as isize), (next_pos.1 - (r.0 as isize)).abs() as usize)
+                },
+                Direction::Up | Direction::Down => {
+                    let r = map
+                        .get(next_pos.1 as usize)
+                        .unwrap()
+                        .iter()
+                        .enumerate()
+                        .find(|(_, &s)| s == Spot::PortalStart(c) )
+                        .unwrap();
+                    ((r.0 as isize, next_pos.1), (next_pos.0 - (r.0 as isize)).abs() as usize)
+                }
+            }
+        }
+        _ => panic!("Cannot teleport through this"),
+    };
+
+    panic!("Wat");
+    state
 }
 
 fn parse_map<'a>(lines: impl Iterator<Item = &'a String>) -> Vec<Vec<Spot>> {
@@ -204,7 +252,7 @@ fn parse_instructions(input: &str) -> Vec<Instruction> {
             match it.next() {
                 Some('L') => Some(Instruction::Counterclockwise),
                 Some('R') => Some(Instruction::Clockwise),
-                Some(_) => panic!("Unknown instruction alphanumeric character"),
+                Some(c) => panic!("Unknown instruction alphanumeric character '{}'", c),
                 None => None,
             }
         })
